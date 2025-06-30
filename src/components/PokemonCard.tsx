fe457a403b2b5afdcd21af5294d9b823e0e5a6a2
Eroma <eugrodmar@gmail.com>
2025-06-30 14:32:03 +0200

    7

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
 
+ interface Pokemon {
+    id: number;
+    name: string;
+}
+
 function PokemonCard(){
 
-    const[pokemon, setPokemon] = useState<any>({});
+    const[pokemon, setPokemon] = useState<Pokemon | null>(null);
 
     useEffect (() => {
         fetch("https://pokeapi.co/api/v2/pokemon/1/")
         .then((response) => response.json())
-        .then((data)=> setPokemon(data))
-    }, [])
+        .then((data)=> setPokemon(data));
+    }, []);
 
-    return(
-        
+    return pokemon.id ? (
         <li className="pokemon-card">
             <h2 className="pokemon-name">{pokemon.name}</h2>
         </li>
-    )
+        )
+    
 }
 
 export default PokemonCard;
